ae9402eb736eba239ac0bbdd406dbed6b23012e7
Jonathan <jonathan@iMac9.local>
2019-04-16 12:39:16 -0400

    Create an app which will show each life cycle on the label on the middlescreen

--- OrderOfEvents/OrderOfEvents/SecondViewController.swift
+++ OrderOfEvents/OrderOfEvents/SecondViewController.swift
@@ -10,12 +10,51 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var LabelSecondScreen: UILabel!
+    
+    var eventNumber: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        if let existingText = LabelSecondScreen.text {
+            LabelSecondScreen.text = "\(existingText)\nEvent number\(eventNumber) was viewDidLoad"
+            eventNumber += 1
+            // Add statement to the label about a life cycle which just occured, print it with the corresponding event number.
+        }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let existingText = LabelSecondScreen.text {
+            LabelSecondScreen.text = "\(existingText)\nEvent number\(eventNumber) was viewWillAppear"
+             eventNumber += 1
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let existingText = LabelSecondScreen.text {
+            LabelSecondScreen.text = "\(existingText)\nEvent number\(eventNumber) was viewDidAppear"
+            eventNumber += 1
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let existingText = LabelSecondScreen.text {
+            LabelSecondScreen.text = "\(existingText)\nEvent number\(eventNumber) was viewWillDisappear"
+            eventNumber += 1
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if let existingText = LabelSecondScreen.text {
+            LabelSecondScreen.text = "\(existingText)\nEvent number\(eventNumber) was viewDidDisappear"
+            eventNumber += 1
+        }
+    }
 
     /*
     // MARK: - Navigation
